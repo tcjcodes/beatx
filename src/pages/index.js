7de9f0bc9b016ec08689264a00361f78672a1acc
Trina <tcjcastillo@gmail.com>
2023-07-28 16:45:35 -0700

    WIP begin styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,35 @@ import React from 'react';
 import Layout from "../components/layout";
 
 const IndexPage = () => {
-    return (
-        <Layout>hello world</Layout>
-    )
+  return (<Layout>
+    <div className="container" style={{
+      height: '200px',
+      position: 'relative',
+      // border: '1px solid grey'
+    }}>
+      <div className="player"
+           style={{
+
+             display: 'flex',
+             justifyContent: 'center',
+             alignItems: 'center',
+             // height: '200px',
+             border: '3px solid green',
+             padding: '2em',
+             textAlign: 'center',
+           }}>
+        <p>hello world</p>
+      </div>
+    </div>
+    {/*<div style={{
+      margin: 'auto',
+      width: '50%',
+      border: '2px solid white',
+      padding: '2em',
+      textAlign: 'center'
+    }}>hello world
+    </div>*/}
+  </Layout>)
 }
 
 export default IndexPage
