46e42f43a1869863bf8baac8d3c214b3d1c7e239
Trina <tcjcastillo@gmail.com>
2023-09-02 22:04:27 -0700

    style: seeker thumb

--- src/components/Seeker.jsx
+++ src/components/Seeker.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, {useState} from 'react';
+import styled, from 'styled-components';
 
 /**
  * Slider CSS adapted from:
@@ -18,11 +18,21 @@ const SliderContainer = styled.div`
   align-items: baseline;
 `;
 
-const Slider = styled.input.attrs(props => ({
+const thumbCss = css`
+  box-shadow: none;
+  border: none;
+  height: 16px;
+  width: 8px;
+  border-radius: 0;
+  background: #ffffff;
+  cursor: pointer;
+  margin-top: -14px; /* You need to specify a margin in Chrome, but in Firefox and IE it is automatic */
+  //box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d; /* Add cool effects to your sliders! */
+`;
+const SliderInput = styled.input.attrs(() => ({
   type: 'range',
   min: 0,
   max: 100,
-  value: props.value,
 }))`
   position: absolute;
   top: -2px;
@@ -35,12 +45,45 @@ const Slider = styled.input.attrs(props => ({
   opacity: 1;
   -webkit-transition: .2s;
   transition: opacity .2s;
+
+  &::-webkit-slider-thumb {
+    box-shadow: none;
+    border: none;
+    height: 16px;
+    width: 8px;
+    border-radius: 0;
+    background: #ffffff;
+    cursor: pointer;
+    margin-top: -14px; /* You need to specify a margin in Chrome, but in Firefox and IE it is automatic */
+    //box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d; /* Add cool effects to your sliders! */
+  }
+
+  &::-moz-range-thumb {
+    box-shadow: none;
+    border: none;
+    height: 16px;
+    width: 8px;
+    border-radius: 0;
+    background: #ffffff;
+    cursor: pointer;
+  }
+
+  &::-ms-thumb {
+    box-shadow: none;
+    border: none;
+    height: 16px;
+    width: 8px;
+    border-radius: 0;
+    background: #ffffff;
+    cursor: pointer;
+  }
 `;
 
 const CompletionBar = styled.div`
   position: relative;
   top: 0;
-  width: 50%;
+  width: ${props => `${props.value}%`};
+
   height: ${SLIDER_HEIGHT_PX}px;
   background-color: rgba(255, 255, 255, 1.0);
   z-index: 0;
@@ -53,17 +96,22 @@ const TimestampContainer = styled.div`
   font-size: 0.85em;
 `;
 
-const Seeker = () => <SeekerContainer>
-  <SliderContainer>
-    <Slider
-        value={26}/>
-    <CompletionBar/>
-  </SliderContainer>
-  <TimestampContainer>
-    <span>01:26</span>
-    <span>02:18</span>
-  </TimestampContainer>
-</SeekerContainer>;
+const Seeker = () => {
+  const [value, setValue] = useState(0);
+
+  return (<SeekerContainer>
+    <SliderContainer>
+      <SliderInput
+          onChange={(e) => setValue(e.target.value)}
+          value={value}/>
+      <CompletionBar value={value}/>
+    </SliderContainer>
+    <TimestampContainer>
+      <span>01:26</span>
+      <span>02:18</span>
+    </TimestampContainer>
+  </SeekerContainer>);
+};
 
 export default Seeker;
 
